caf6c952001dfd060f7ff647ed7c4c603b57ef47
Kim Gwang woon <misili304@naver.com>
2023-04-30 14:21:10 +0900

    Add/ Task Model

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include("users.urls")),
-
+    path('tasks/', include("tasks.urls")),
 ]
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=255)
+    is_complete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
